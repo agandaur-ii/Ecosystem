0dc5bbdfe8f17dfde78e8881e50bd36bde61ce4e
Chris Wagstaffe <cjwagstaffe@gmail.com>
2024-02-08 01:13:17 -0800

    plants now spawn in at appropriate size

--- Assets/Scripts/Behaviour/Plant.cs
+++ Assets/Scripts/Behaviour/Plant.cs
@@ -17,13 +17,13 @@ public class Plant : LivingEntity {
     protected virtual void SpawnNewPlants()
     {
         var numberOfOffspring = RandomNumberOfOffspring();
-        Debug.Log("Number of Offspring: " + numberOfOffspring);
         // Get coord of plant
         var currentCoord = coord;
         // Get nearby walkable coords, depending on numberOfOffspring
         var listOfNearbyWalkableCoords = Environment.GetNextTileRandom(currentCoord);
         // Spawn plants at those coords
         var entity = Instantiate(this);
+        entity.transform.localScale = new Vector3(1, 1, 1);
         entity.Init(listOfNearbyWalkableCoords);
         Environment.speciesMaps[entity.species].Add(entity, listOfNearbyWalkableCoords);
 
